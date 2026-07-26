e3ac734558024dda575fa4e76eaf87f0fdd71ac4
Kingsoft0001 <pankajking67@gmail.com>
2026-07-27 00:10:34 +0530

    Add WhiteNoise for static files

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_DIR=BASE_DIR/"static"
 STATICFILES_DIRS=[STATIC_DIR]
