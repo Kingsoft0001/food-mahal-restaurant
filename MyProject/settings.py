c48aaa14b20c63012396dec9ee5397523863c1f0
Kingsoft0001 <pankajking67@gmail.com>
2026-07-27 20:25:47 +0530

    Secure Django secret key

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -24,10 +24,13 @@ TEMPLATE_DIR=BASE_DIR/"templates"
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get ("django-insecure-fl5=l*nc)!-rudj$i#-rnva81^u_7cg+)&a-n_gfj@vbtpw!sp")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-local-development-key"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'food-mahal-restaurant.onrender.com']
 
