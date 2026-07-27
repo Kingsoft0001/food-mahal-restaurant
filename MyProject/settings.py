6a658255037345e3410c7490563eed4244b3c020
Kingsoft0001 <pankajking67@gmail.com>
2026-07-27 20:02:57 +0530

    Secure Django secret key

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for MyProject project.
 
@@ -22,7 +24,7 @@ TEMPLATE_DIR=BASE_DIR/"templates"
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fl5=l*nc)!-rudj$i#-rnva81^u_7cg+)&a-n_gfj@vbtpw!sp'
+SECRET_KEY = os.environ.get ("django-insecure-fl5=l*nc)!-rudj$i#-rnva81^u_7cg+)&a-n_gfj@vbtpw!sp")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
